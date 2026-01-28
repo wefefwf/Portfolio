b50a26d99b5124b763b696c20800c4d3383d0ca3
= <=>
2026-01-28 16:45:06 +0900

    init

---
+++ src/main/java/com/springbootsmini/app/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.springbootsmini.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		
+		registry.addViewController("/").setViewName("views/mainPage");
+		registry.addViewController("main").setViewName("views/mainPage");
+		registry.addViewController("portfolio").setViewName("views/mainPage");
+		
+	}	
+}
